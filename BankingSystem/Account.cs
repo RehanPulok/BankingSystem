e3f123a60f68c375a78cd879682e62b6ede5835b
18-37907-2@student.aiub.edu <18-37907-2@student.aiub.edu>
2021-03-02 04:21:05 +0600

    Account class added

---
+++ BankingSystem/Account.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingSystem
+{
+    class Account
+    {
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        Address address;
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+            set { this.accountNumber = 1000; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public double Balance 
+        {
+
+            set { this.balance = value; }
+            get { return this.balance; }
+        }
+        public Account(string accountName, double balance)
+        {
+            this.accountName = accountName;
+            this.balance = balance;
+            this.accountNumber++;
+         
+
+        }
+        
+    }
+}
